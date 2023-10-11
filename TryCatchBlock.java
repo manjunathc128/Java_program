1e94871633aa72f9c62f7ff3377f749bea84d52e
Manjunath C <cmanjunath128@gmail.com>
2023-10-11 21:04:25 +0530

    Update TryCatchBlock.java

--- TryCatchBlock.java
+++ TryCatchBlock.java
@@ -29,3 +29,38 @@ public class Main{
     }
   }
 }
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args){
+	    int [ ] arr = new int[3];
+	    arr[0] = 7;
+        arr[1] = 56;
+        arr[2] = 6;
+	    boolean flag = true;
+	    Scanner sc = new Scanner(System.in);
+	    while(flag){
+	        System.out.println("Please Enter an index");
+	        int ind = sc.nextInt();
+	        try{
+	            
+	            System.out.println("Try Gussing your luck");
+	            try{
+	                System.out.println(arr[ind]);
+	                flag = false;
+	            }
+	            catch(ArrayIndexOutOfBoundsException e){
+	                System.out.println("PLease enter valid index");
+	                ind = sc.nextInt();
+	                
+	            }
+	        }
+	        catch(Exception e){
+	           System.out.println("general error");
+	       }     
+	    
+	    }
+	}
+}
